f1ffc083d605a6be2664f0cc5c29ed1ab9526039
Alex Koenig <alex.koenig4@gmail.com>
2020-09-30 23:24:30 -0400

    More production preparation

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://akoenig1:Rcbbpw2s@cluster0.rsk6g.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://akoenig1:Rcbbpw2s@cluster0.rsk6g.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
